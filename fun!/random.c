4bacf5a7400846d2b40a41695d492f03eadca42e
yuanbo@42 <noemail@example.com>
2018-08-17 11:24:54 +0800

    the real-time random 	修改：      fun!/random.c

--- fun!/random.c
+++ fun!/random.c
@@ -1,47 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void enjoyEnter(char pool[]);/* enjoy pressing ENTER */
-void statistic(char pool[]);/* statistic of 10k times of rand	*/
+char out_pool[] = {'A','B','C','D'};
+int seeded=0;
 
-void enjoyEnter(char pool[])
+void enjoy_enter();/* enjoy pressing ENTER */
+void statistic();/* statistic of 10k times of rand	*/
+void get_seed();/* real-time random needs real time seed */
+
+void enjoy_enter()
 {
+	int pool_size = sizeof(out_pool) / sizeof(char);
+	printf("Start with pressing Enter!\n");
 	while(getchar() != EOF){
 		printf("You get a random: ");
-		int index = rand() % strlen(pool);
-		if(pool[index] == 'a'){
-			printf("a grade, GOOD!\n");
+		get_seed();
+		int index = rand() % pool_size;
+		if(out_pool[index] == 'A'){
+			printf("Grade A, good!\n");
 		}
 		else{
-			printf("%c\n", pool[index]);	
+			printf("%c\n", out_pool[index]);	
 		}
 		
 	}	
 }
 
-void statistic(char pool[])
+void statistic()
 {
 	int times = 10000;
 	int i;
 	int eachtimes[1000] = {0};
+	int pool_size = sizeof(out_pool)/sizeof(out_pool[0]);
 	for(i = 0; i < times; i++){
-		eachtimes[rand() % strlen(pool)] ++;
+		get_seed();
+		eachtimes[rand() % pool_size] ++; //TODO not real-time random
 	}
 	printf("times appears of each element :  \n");
-	for(i = 0; i < strlen(pool); i++){
+	for(i = 0; i < strlen(out_pool); i++){
+		printf("%c:\t", out_pool[i]);
 		printf("%d\n", eachtimes[i]);
+	}
+}
+
+void get_seed()
+{
+	if(!seeded){
+		struct timeval time;
+		gettimeofday(&time, NULL);
+		srand(time.tv_usec);
+		seeded = 1;
+	}
+	else{
+		;
 	}
 }
 
 /* get a random char string	*/
 int main()
 {
-	char pool[] =
-	  { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-        'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; 
-	//statistic(pool);
-	//enjoyEnter(pool);
+	statistic();
+	//enjoy_enter();
 }
